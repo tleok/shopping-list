55e49b0b1e35247958c3ed1c64710fc7d48d42ab
Tanner Kay <tleok100@gmail.com>
2019-11-04 20:43:32 -0700

    started the functions on the javascript to grab the input on submit

---
+++ index.js
@@ -0,0 +1,25 @@
+function addNewItem() {
+    $('#js-shopping-list-form').submit(function(event){
+        event.preventDefault();
+        $('#shopping-list-entry').empty();
+        const newListItem = $(event.currentTarget).find('#shopping-list-entry');
+        $(".js-display-user-text").text(`user text is:  ${newListItem.val()}`);/*adding this to display the text, I need to figure out how to add this to the UL items rather than displaying on the screen */
+    })
+}
+
+$(addNewItem)
+/*
+$(function() {
+  
+  $("button").click(function(event) {
+    $("ul").append(
+      "<li>" +
+      ["cat", "dog", "rock"][Math.floor(Math.random()*3)] + "</li>"
+    );
+  });
+  
+  $('ul').on('click', 'li', function(event) {
+    this.remove();
+  });
+});
+*/
