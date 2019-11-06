3f74cf5e9a8923951a72167fa9733d6bda2e77fc
Tanner Kay <tleok100@gmail.com>
2019-11-05 17:02:21 -0700

    working on the check button

--- index.js
+++ index.js
@@ -3,7 +3,11 @@ function addNewItem() {
     $('#js-shopping-list-form').submit(function(event){
         event.preventDefault();
         $('#shopping-list-entry').empty();
-        const newListItem = $(event.currentTarget).find('#shopping-list-entry');
+        
+        //$('ul').on('click', 'li', function(event) {
+          //this.remove();
+        
+          const newListItem = $(event.currentTarget).find('#shopping-list-entry');
         $(".shopping-list").append(`<li><span class="shopping-item"> ${newListItem.val()}</span>
           <div class="shopping-item-contols"><button class="shopping-item-toggle">
           <span class="button-label">check</span>
@@ -16,22 +20,23 @@ function addNewItem() {
 //invoke the Function on Ready
 $(addNewItem)
 
+
 function itemChecked() {
-  $('#js-shopping-list-form').submit(function(event){
-      event.preventDefault();
-      $('#shopping-list-entry').empty();
-      const newListItem = $(event.currentTarget).find('#shopping-list-entry');
-      $(".shopping-list").append(`<li><span class="shopping-item"> ${newListItem.val()}</span>
-        <div class="shopping-item-contols"><button class="shopping-item-toggle">
-        <span class="button-label">check</span>
-        </button>
-        <button class="shopping-item-delete">
-        <span class="button-label">delete</span>
-        </button></div> </li>`);/*adding this to display the text, I need to figure out how to add this to the UL items rather than displaying on the screen */
+  $('.shopping-item-toggle').on('click', function(event){
+    const checkedItem = $(event.currentTarget);
+    checkedItem.addClass('shopping-item__checked');
   })
+    //$('ul').on('click', 'li', function(event) {
+      //this.remove();
+    //});
 }
-//invoke the Function on Ready
 $(itemChecked)
+/*  Toggle the checkbox
+$('#choose_address2').click(function(){
+   $('.toggle').each(function(){
+     $(this).prop('checked', !$(this)[0].checked);
+  })
+}) */
 
 
 
